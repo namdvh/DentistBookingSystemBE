5eeb8ddedbfd8928fd6618dbd08cbad658bb4a8d
Anh Khoa <anhkhoahuynh90@gmail.com>
2022-06-15 00:37:39 +0700

    profile for dentist

---
+++ DentistBooking.ViewModels/System/Users/ProfileDTO.cs
@@ -0,0 +1,28 @@
+﻿using DentisBooking.Data.Entities;
+using DentisBooking.Data.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentistBooking.ViewModels.System.Users
+{
+    public class ProfileDTO
+    {
+        public Guid Id { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime DOB { get; set; }
+        public string? ImageUrl { get; set; }
+        public int? DentistId { get; set; }
+        public Position Position { get; set; }
+        public string Description { get; set; }
+
+
+
+    }
+}
